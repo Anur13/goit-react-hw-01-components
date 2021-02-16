c135cc44398e15648bb919ae1a7d6eb07a538a9c
Anur13 <72020529+Anur13@users.noreply.github.com>
2021-02-16 15:27:46 +0200

    Delete src/components directory

--- src/components/User-Section/UserSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import userInfo from '../Profile/user.json';
-import Profile from '../Profile/Profile';
-
-const UserSection = () => {
-  return <Profile key={userInfo.name} {...userInfo} />;
-};
-
-export default UserSection;
